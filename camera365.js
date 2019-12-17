cdcecec2465f1e0b242e4194bc9b47ecd581c4a2
zack <zackzeng00@gmail.com>
2019-12-18 00:35:37 +0800

    add new

---
+++ camera365.js
@@ -0,0 +1,23 @@
+#camera360&mix
+^https:\/\/bmall\.camera360\.com\/api\/(iap\/check-receipt$|mix\/getinfo$|mix\/recovery$) url script-response-body camera360.vip.js
+
+*/
+var obj = JSON.parse($response.body);
+obj=
+{
+  "status": 200,
+  "data": {
+    "sandbox": 1,
+    "purchaseTime": 1571682188,
+    "giftVip": 0,
+    "productId": "VIP_yearly_29.99",
+    "appleVip": 1,
+    "expireTime": 4096532852,
+    "operationVip": 1,
+    "errorCode": 0
+  },
+  "message": "ok",
+  "exetime": "1572056389212-1572056391362",
+  "serverTime": 1572056391.3622
+};
+$done({body: JSON.stringify(obj)});
---
+++ drafts5.js
@@ -0,0 +1,16 @@
+/*backend.getdrafts.com
+
+http-response https:\/\/backend\.getdrafts\.com\/api\/.*\/verification* requires-body=1,max-size=0,script-path=js/draft.js
+*/
+var obj = JSON.parse($response.body);
+
+obj= {
+  "active_expires_at" : "2099-01-01T00:00:00Z",
+  "is_subscription_active" : true,
+  "active_subscription_type" : "premium",
+  "is_blocked" : false
+};
+
+$done({body: JSON.stringify(obj)});
+
+// Descriptions
