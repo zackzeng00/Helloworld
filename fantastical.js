5e5105be1493636284f23dfd565d6cb5e6d9fd45
sw000776 <sw000776@shuweideMacBook-Pro.local>
2024-01-23 17:35:31 +0800

    更新
    更新日历app，验证中

--- fantastical.js
+++ fantastical.js
@@ -1,41 +1,29 @@
 /*
 ^https:\/\/api\.flexibits\.com\/v1\/(auth|account)\/(device|details|appstore-receipt)\/$ url script-response-body fantastical.js
 hostname=api.flexibits.com
+2023-01-23 updata
+http-response ^https:\/\/api\.flexibits\.com\/(v1\/auth\/device|v1\/account\/details) requires-body=1,script-path=https://raw.githubusercontent.com/deezertidal/private/master/js-backup/Crack/fantastical.js
 */
 
-const path1="device";
-const path2="details";
-const path3="appstore-receipt";
-let url=$request.url;
-let obj=JSON.parse($response.body);
-
-if(url.indexOf(path1)!=-1){
-
-  obj.subscription.autorenew=true;
-  obj.subscription.expiration="2099-02-01T16:49:37Z";
-  obj.subscription.expires="2099-02-01T16:49:37Z";
-  obj.scope=["notify", "weather", "keyvalue-watch", "keyvalue-verification", "schedjoules", "scheduling", "account", "keyvalue", "fantastical"];
-
+let url = $request.url;
+const path1="v1/auth/device";
+const path2="v1/account/details";
+let obj = JSON.parse($response.body);
+if (url.indexOf(path1) != -1) {
+obj["subscription"]={
+  "autorenew": true,
+  "expiration": "2099-02-15T09:09:14Z",
+  "expires": "2099-02-15T09:09:14Z"
+ }
 }
-
-
-if(url.indexOf(path2)!=-1){
-
-  obj.subscription.autorenew=true;
-  obj.subscription.expiration="2099-02-01T16:49:37.000000Z";
-  obj.subscription.subscription_type="AppStore";
-  obj.subscription.is_expired=false;
-  obj.subscription.trial=false;
-    
+if (url.indexOf(path2) != -1) {
+obj["subscription"]={
+  "autorenew": true,
+  "expiration": "2099-02-15T09:09:14.000000Z",
+  "uuid": "3c5927a6-4e3e-4773-b7af-62506e036dc7",
+  "subscription_type": "AppStore",
+  "is_expired": false,
+  "trial": false
+ }
 }
-
-if(url.indexOf(path3)!=-1){
-	
-	obj.autorenew=true;
-	obj.expiration="2099-02-01T16:49:37.000000Z";
-	obj.subscription_type="AppStore";
-	obj.is_expired=false;
-	obj.trial=false;
-}
-
-$done({body:JSON.stringify(obj)});
+$done({body: JSON.stringify(obj)});
